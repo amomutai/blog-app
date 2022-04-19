cce324f7b3d39706fee24305cd29196b14ad01e5
Amos Mutai <amosmutai04@gmail.com>
2022-04-19 13:12:54 +0300

    new

---
+++ frontend-new/src/App.js
@@ -0,0 +1,16 @@
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route  path='/' element={<Home/>}/>
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
